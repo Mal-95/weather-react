b89c9960b4f947d8b79d7fa4bfceab23dab1db8a
Mal-95 <malebo445.ml@gmail.com>
2024-05-26 21:49:31 +0200

    Added spaces

--- src/Weather.js
+++ src/Weather.js
@@ -53,9 +53,9 @@ export default function Weather() {
             <a href="https://github.com/Mal-95" className="GithubLink" target="_blank" rel="noreferrer">
                 <strong>Malebo Letsoalo</strong>
                 </a>
-            and is on 
+            and is on  {" "}
             <a href="https://github.com/Mal-95/weather-react" className="RepoLink" target="_blank" rel="noreferrer"><strong>GitHub</strong></a>
-            and hosted on
+            and hosted on {" "}
             <a href="https://app.netlify.com/sites/weather-react-app-challenge" target="_blank" rel="noreferrer"><strong>Netlify</strong></a>
         </footer>
       </div>
