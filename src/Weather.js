429163feccb41c2300ea12ab8b02f70842e671af
Mal-95 <malebo445.ml@gmail.com>
2024-05-26 21:47:25 +0200

    Added links for hosting

--- src/Weather.js
+++ src/Weather.js
@@ -50,10 +50,13 @@ export default function Weather() {
         </ul>
         <footer>
             This project is coded by 
-            <a href="https://github.com/Mal-95" className="GithubLink" target="blank">
+            <a href="https://github.com/Mal-95" className="GithubLink" target="_blank" rel="noreferrer">
                 <strong>Malebo Letsoalo</strong>
                 </a>
-            
+            and is on 
+            <a href="https://github.com/Mal-95/weather-react" className="RepoLink" target="_blank" rel="noreferrer"><strong>GitHub</strong></a>
+            and hosted on
+            <a href="https://app.netlify.com/sites/weather-react-app-challenge" target="_blank" rel="noreferrer"><strong>Netlify</strong></a>
         </footer>
       </div>
     );
